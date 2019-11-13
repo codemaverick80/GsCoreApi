731a84cbafbc0aada38523e0e5b0f45302208c3b
Codemaverick <codemaverick@outlook.com>
2019-11-12 20:15:38 -0800

    Repository added in GsCore.Api under services folder

---
+++ GsCoreApi/GsCore.Api/V1/Services/Repository/AlbumRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GsCore.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GsCore.Api.V1.Services.Repository
+{
+    public class AlbumRepository:IAlbumRepository
+    {
+        private readonly GsDbContext _context;
+
+        public AlbumRepository(GsDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Album> GetAlbum(int albumId)
+        {
+            IQueryable<Album> query = _context.Set<Album>();
+            
+            //// Build Query
+            var result = query.Where(a => a.Id == albumId);
+
+
+            //// execute query and return the result
+            return await result.FirstOrDefaultAsync();
+
+        }
+
+        public async Task<IEnumerable<Album>> GetAlbums(int artistId)
+        {
+            IQueryable<Album> query = _context.Set<Album>();
+
+            //// Build Query
+            var result = query.Where(a => a.ArtistId == artistId);
+
+            //// execute query and return the result
+            return await result.ToListAsync();
+        }
+
+        public void AddAlbum(int artistId,int genreId, Album album)
+        {
+            if (album == null)
+            {
+                throw new ArgumentNullException(nameof(album));
+            }
+
+            album.ArtistId = artistId;
+            album.GenreId = genreId;
+            _context.Add(album);
+        }
+
+
+        public async Task<bool> Save()
+        {
+            return ( await _context.SaveChangesAsync() > 0);
+        }
+
+
+
+        #region "Disposing"
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+       
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context?.Dispose();
+            }
+        }
+
+        
+
+
+
+
+        #endregion
+
+    }
+}
---
+++ GsCoreApi/GsCore.Api/V1/Services/Repository/IAlbumRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GsCore.Database.Entities;
+
+namespace GsCore.Api.V1.Services.Repository
+{
+    public interface IAlbumRepository:IDisposable
+    {
+
+      Task<IEnumerable<Album>> GetAlbums(int artistId);
+
+      Task<Album> GetAlbum(int albumId);
+
+      void AddAlbum(int artistId, int genreId, Album album);
+
+      Task<bool> Save();
+    }
+}
