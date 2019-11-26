c07ef9d6aa4c29a1a96d2d5658ed6d9654f72913
Codemaverick <codemaverick@outlook.com>
2019-11-25 16:42:47 -0800

    Update and delete contracts added

---
+++ GsCoreApi/GsCore.Api/V1/Contracts/Requests/GenreUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GsCore.Api.V1.Contracts.Requests
+{
+    public class GenreUpdateRequest
+    {
+    }
+}
